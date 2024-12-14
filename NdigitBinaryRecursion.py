63cb15efe1277de9b1f1aea74378e390c6741325
ohousar <ondrej.housar@generalbytes.com>
2024-12-14 22:49:11 +0100

    optimized

--- NdigitBinaryRecursion.py
+++ NdigitBinaryRecursion.py
@@ -1,12 +1,9 @@
-def nDigitBinary(number, n, k):
-    if k == n:
+def nDigitBinary(number, n, depth):
+    if depth == n:
         print(number)
-    elif k == 1:
-        if n == 1:
-            print("1")
-        else:
-            nDigitBinary("1",n,k+1)
+    elif depth == 1:
+        nDigitBinary("1", n, depth+1)
     else:        
-        nDigitBinary(number + "0",n,k+1)
-        nDigitBinary(number + "1",n,k+1)
+        nDigitBinary(number + "0", n, depth+1)
+        nDigitBinary(number + "1", n, depth+1)
 nDigitBinary("", 5, 1)
